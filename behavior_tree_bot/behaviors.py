6b68761d3841b545049ceff22f46a9b6c679a5f3
dmitriusagoston <dmitri.agoston@gmail.com>
2024-01-27 17:12:13 -0800

    currently beats all 5 bots in sim

--- behavior_tree_bot/behaviors.py
+++ behavior_tree_bot/behaviors.py
@@ -5,14 +5,20 @@ from planet_wars import issue_order
 
 def attack_weakest_enemy_planet(state):
     # (1) If we currently have a fleet in flight, abort plan.
-    if len(state.my_fleets()) >= 1:
-        return False
+    # if len(state.my_fleets()) >= 1:
+    #     return False
+    
+    current_destinations = [fleet.destination_planet for fleet in state.my_fleets()]
+    untargeted_enemy_planets = [planet for planet in state.enemy_planets() if planet.ID not in current_destinations]    
 
     # (2) Find my strongest planet.
     strongest_planet = max(state.my_planets(), key=lambda t: t.num_ships, default=None)
 
-    # (3) Find the weakest enemy planet.
-    weakest_planet = min(state.enemy_planets(), key=lambda t: t.num_ships, default=None)
+    # find the weakest enemy planet without a fleet in flight
+    weakest_planet = min(untargeted_enemy_planets, key=lambda t: t.num_ships, default=None)
+    
+    
+    #weakest_planet = min(state.enemy_planets(), key=lambda t: t.num_ships, default=None)
 
     if not strongest_planet or not weakest_planet:
         # No legal source or destination
@@ -39,3 +45,21 @@ def spread_to_weakest_neutral_planet(state):
     else:
         # (4) Send half the ships from my strongest planet to the weakest enemy planet.
         return issue_order(state, strongest_planet.ID, weakest_planet.ID, strongest_planet.num_ships / 2)
+
+def capture_best_neutral_planet(state):
+    my_planets = state.my_planets()
+    if not my_planets:
+        return False
+
+    current_destinations = [fleet.destination_planet for fleet in state.my_fleets()]
+    untargeted_neutral_planets = [planet for planet in state.neutral_planets() if planet.ID not in current_destinations]
+    if not untargeted_neutral_planets:
+        return False
+
+    best_planet = min(untargeted_neutral_planets, key=lambda p: (-p.growth_rate, min(state.distance(p.ID, my_p.ID) for my_p in my_planets), p.num_ships))
+    strongest_planet = max(my_planets, key=lambda p: p.num_ships)
+
+    # find the minimum ships to send
+    ships = best_planet.num_ships + 1
+
+    return issue_order(state, strongest_planet.ID, best_planet.ID, ships)
--- behavior_tree_bot/bt_bot.py
+++ behavior_tree_bot/bt_bot.py
@@ -26,17 +26,24 @@ def setup_behavior_tree():
     # Top-down construction of behavior tree
     root = Selector(name='High Level Ordering of Strategies')
 
+    # Capture Quality Neutral Planets
+    quality_plan = Sequence(name='Capture Quality Neutral Planets')
+    neutral_planet_check = Check(if_neutral_planet_available)
+    capture_action = Action(capture_best_neutral_planet)
+    quality_plan.child_nodes = [neutral_planet_check, capture_action]
+
     offensive_plan = Sequence(name='Offensive Strategy')
     largest_fleet_check = Check(have_largest_fleet)
     attack = Action(attack_weakest_enemy_planet)
     offensive_plan.child_nodes = [largest_fleet_check, attack]
 
-    spread_sequence = Sequence(name='Spread Strategy')
-    neutral_planet_check = Check(if_neutral_planet_available)
-    spread_action = Action(spread_to_weakest_neutral_planet)
-    spread_sequence.child_nodes = [neutral_planet_check, spread_action]
+    # spread_sequence = Sequence(name='Spread Strategy')
+    # neutral_planet_check = Check(if_neutral_planet_available)
+    # spread_action = Action(spread_to_weakest_neutral_planet)
+    # spread_sequence.child_nodes = [neutral_planet_check, spread_action]
 
-    root.child_nodes = [offensive_plan, spread_sequence, attack.copy()]
+    # root.child_nodes = [offensive_plan, spread_sequence, attack.copy()]
+    root.child_nodes = [offensive_plan, quality_plan, attack.copy(), capture_action.copy()]
 
     logging.info('\n' + root.tree_to_string())
     return root
